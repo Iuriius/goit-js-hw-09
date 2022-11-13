3fecb55074dc858746ef0265de3a3c2096ffd475
Iuriius <105295809+Iuriius@users.noreply.github.com>
2022-11-13 22:35:04 +0000

    Deploying to gh-pages from @ Iuriius/goit-js-hw-09@38bcffb3a9a2e53ed013e5d64c67235e42010fa2 🚀

--- 03-promises.6562b6d3.js
+++
@@ -1,2 +0,0 @@
-(Math.random(),void setTimeout((function(){isSuccess?resolve("success value"):reject("error")}),2e3)).then((function(o){var e=o.position,c=o.delay;console.log("✅ Fulfilled promise ".concat(e," in ").concat(c,"ms"))})).catch((function(o){var e=o.position,c=o.delay;console.log("❌ Rejected promise ".concat(e," in ").concat(c,"ms"))}));
-//# sourceMappingURL=03-promises.6562b6d3.js.map
