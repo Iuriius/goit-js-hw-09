cd782960d1fb41f3d5edf635094bdbb50aaa0903
Iuriius <105295809+Iuriius@users.noreply.github.com>
2022-11-13 14:10:18 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,13 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   setTimeout(() => {
-    // Change value of isSuccess variable to simulate request status
-    const isSuccess = true;
-
     if (isSuccess) {
       resolve("success value");
     } else {
       reject("error");
     }
   }, 2000);
-});
-}
+};
 
 createPromise(2, 1500)
   .then(({ position, delay }) => {
